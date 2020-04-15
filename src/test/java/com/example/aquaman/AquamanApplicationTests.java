e4614f4b1c12071bfa9586fc6372bb6dd36f2253
chenjianfeng <chenjianfeng@163.cn>
2020-04-15 10:02:05 +0800

    初始化 website

---
+++ src/test/java/com/example/aquaman/AquamanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aquaman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquamanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
